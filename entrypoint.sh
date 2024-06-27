c821b1760900c34dd690ffa2fe26196554c7aa91
deniputraa <57161964+deniputraa@users.noreply.github.com>
2024-06-27 13:16:06 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ DESTINATION="/var/www/html/dashboard-gateway"
 # Cek apakah folder storage sudah ada di tujuan
 if [ ! -d "$DESTINATION/public/storage" ]; then
     echo "Folder storage tidak ditemukan di $DESTINATION, menyalin folder..."
-    #rsync -av --delete "$SOURCE/" "$DESTINATION"
+    rsync -av --delete "$SOURCE/" "$DESTINATION"
 else
     echo "Folder storage sudah ada di $DESTINATION, menyalin kecuali /storage..."
-    #rsync -av --delete --exclude=/storage "$SOURCE/" "$DESTINATION"
+    rsync -av --delete --exclude=/storage "$SOURCE/" "$DESTINATION"
 fi
