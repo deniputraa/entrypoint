50c8ee0ef53f2e1e7f9fa0748ee18519f90861ff
deniputraa <57161964+deniputraa@users.noreply.github.com>
2024-06-27 11:06:21 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,6 @@ if [ ! -d "$DESTINATION/storage" ]; then
     echo "Folder storage tidak ditemukan di $DESTINATION, menyalin folder..."
     rsync -av --delete "$SOURCE/" "$DESTINATION"
 else
+    echo "Folder storage sudah ada di $DESTINATION, menyalin kecuali /storage..."
     rsync -av --delete --exclude=/storage "$SOURCE/" "$DESTINATION"
 fi
