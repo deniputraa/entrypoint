0323aadc47c7b1403c1f25b5b63debf96c6ac2a1
deniputraa <57161964+deniputraa@users.noreply.github.com>
2024-06-27 15:59:28 +0700

    Update entrypoint-universe-dashboard.sh

--- entrypoint-universe-dashboard.sh
+++ entrypoint-universe-dashboard.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# Path sumber dan tujuan
+SOURCE="/home/sourcecode/"
+STORAGE="/home/sourcecode/public/storage"
+DESTINATION="/var/www/html/universe-dashboard"
+
+# Cek apakah folder storage sudah ada di tujuan
+if [ ! -d "$DESTINATION/public/storage" ]; then
+    echo "Folder storage tidak ditemukan di $DESTINATION, menyalin folder..."
+    rsync -av --delete "$SOURCE/" "$DESTINATION"
+else
+    echo "Folder storage sudah ada di $DESTINATION, menyalin kecuali /storage..."
+    rsync -av --delete --exclude=/storage "$SOURCE/" "$DESTINATION"
+fi
